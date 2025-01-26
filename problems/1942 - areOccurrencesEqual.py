45788a411df548c77c5c76c89175b2009a1ee5e6
Fahd <asadmansa@icloud.com>
2025-01-25 22:11:33 -0500

    1942 -py

---
+++ problems/1942 - areOccurrencesEqual.py
@@ -0,0 +1,4 @@
+from collections import Counter
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        return len(set(Counter(s).values())) == 1
--- problems/2248 - intersection.py
+++ problems/2248 - intersection.py
@@ -1,3 +1,19 @@
+#1
+from collections import defaultdict
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        n = len(nums)
+        count_map = defaultdict(int)
+        res = []
+        res_set = set()
+        for arr in nums:
+            for val in set(arr):
+                count_map[val] += 1
+                if count_map[val] == n:
+                    res_set.add(val)
+            
+        return sorted(res_set)
+#2
 from collections import defaultdict
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
