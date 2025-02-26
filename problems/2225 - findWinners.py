831b0ffc338da150dce88af046dd0afd158a195e
Live <asadmansa@icloud.com>
2025-02-26 15:21:05 -0500

    2225

--- problems/2225 - findWinners.py
+++ problems/2225 - findWinners.py
@@ -1,6 +1,25 @@
 from collections import defaultdict
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        losers = defaultdict(int)
+        for match in matches:
+            winner, loser = match
+            losers[loser] += 1
+            if winner not in losers:
+                losers[winner] = 0
+        
+        winners = sorted([key for key,val in losers.items() if val == 0])
+        losers = sorted([key for key,val in losers.items() if val == 1])
+
+        return [
+            winners, 
+            losers
+        ]
+
+
+from collections import defaultdict
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
         winners = set()
         losers = defaultdict(int)
 
